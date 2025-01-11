9f897ead9b268b3f8f304bf7d960f7575f875da9
nekiwo <58634064+nekiwo@users.noreply.github.com>
2025-01-11 15:31:08 -0600

    sveltekit setup

---
+++ config/locations.js
@@ -0,0 +1,32 @@
+export default {
+    "standard": [
+        {
+            "image": "test.png",
+            "location": {
+                "x": 0,
+                "y": 0
+            },
+        }
+    ],
+    "time_travel": [
+        {
+            "image": "test.png",
+            "location": {
+                "x": 0,
+                "y": 0
+            },
+            "date": {
+                "year": 0,
+                "month": 0,
+                "day": 0
+            }
+        }
+    ],
+    "weekly": {
+        "image": "test.png",
+        "location": {
+            "x": 0,
+            "y": 0
+        },
+    }
+}
---
+++ config/settings.js
@@ -0,0 +1,10 @@
+export default {
+    "standard": {
+        "rounds": 5,
+        "time": 180
+    },
+    "time_travel": {
+        "rounds": 5,
+        "time": 180
+    }
+}
